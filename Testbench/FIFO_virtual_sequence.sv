688f978738bb89f727dcdc0d88ffd8d3d07a9859
Daniel <danielcazacu205@gmail.com>
2026-07-29 09:15:30 +0300

    correct:names of classes wr_sequence -> wr_seq | read_sequence -> rd_seq

--- Testbench/FIFO_virtual_sequence.sv
+++ Testbench/FIFO_virtual_sequence.sv
@@ -6,9 +6,9 @@ class FIFO_virtual_sequence extends uvm_sequence;
     `uvm_object_utils(FIFO_virtual_sequence)
     `uvm_declare_p_sequencer(FIFO_virtual_sequencer)
 
-    FIFO_wr_sequence wr_seq;
+    FIFO_wr_seq wr_seq;
 
-    FIFO_rd_sequence rd_seq;
+    FIFO_rd_seq rd_seq;
 
     function new(string name = "FIFO_virtual_sequence");
  
@@ -18,9 +18,9 @@ class FIFO_virtual_sequence extends uvm_sequence;
     
     task pre_body();
 
-        wr_seq = FIFO_wr_sequence :: type_id :: create("wr_seq");
+        wr_seq = FIFO_wr_seq :: type_id :: create("wr_seq");
 
-        rd_seq = FIFO_rd_sequence :: type_id :: create("rd_seq");
+        rd_seq = FIFO_rd_seq :: type_id :: create("rd_seq");
       
     endtask
 
